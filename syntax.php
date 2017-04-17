73afc4ea55cbe7b794e34918045a16de02877056
Satoshi Sahara <sahara.satoshi@gmail.com>
2017-04-17 21:33:52 +0900

    use array $m keeping $match as string

--- syntax.php
+++ syntax.php
@@ -40,56 +40,56 @@ class syntax_plugin_imagebox extends DokuWiki_Syntax_Plugin {
     function handle($match, $state, $pos, Doku_Handler $handler) {
         switch($state){
             case DOKU_LEXER_ENTER:
-                $match=Doku_Handler_Parse_Media(substr($match,3));
-                $match['w'] = $match['width'];
-                $dispMagnify = ($match['w'] || $match['height']) && $this->getConf('display_magnify')=='If necessary' || $this->getConf('display_magnify')=='Always';
+                $m = Doku_Handler_Parse_Media(substr($match,3));
+                $m['w'] = $m['width'];
+                $dispMagnify = ($m['w'] || $m['height']) && $this->getConf('display_magnify')=='If necessary' || $this->getConf('display_magnify')=='Always';
 
                 $gimgs = false;
-                list($src,$hash) = explode('#',$match['src'],2);
+                list($src, $hash) = explode('#', $m['src'], 2);
 
-                if ($match['type'] == 'internalmedia') {
+                if ($m['type'] == 'internalmedia') {
                     global $ID;
                     $exists = false;
                     resolve_mediaid(getNS($ID), $src, $exists);
 
                     if ($dispMagnify) {
-                        $match['detail'] = ml($src,array('id'=>$ID,'cache'=>$match['cache']),($match['linking']=='direct'));
+                        $m['detail'] = ml($src,array('id'=>$ID,'cache'=>$m['cache']),($m['linking']=='direct'));
                         if ($hash) {
-                            $match['detail'] .= '#'.$hash;
+                            $m['detail'] .= '#'.$hash;
                         }
                     }
 
                     if ($exists) $gimgs = @getImageSize(mediaFN($src));
                 } else {
                     if ($dispMagnify) {
-                        $match['detail'] = ml($src,array('cache'=>'cache'),false);
+                        $m['detail'] = ml($src, array('cache'=>'cache'), false);
                         if ($hash) {
-                            $match['detail'] .= '#'.$hash;
+                            $m['detail'] .= '#'.$hash;
                         }
                     }
 
                     $gimgs = @getImageSize($src);
                 }
 
-                $match['exist'] = $gimgs!==false;
+                $m['exist'] = ($gimgs !== false);
 
-                if (!$match['w'] && $match['exist']) {
-                    ($match['height'])?
-                    $match['w'] = $match['height']*$gimgs[0]/$gimgs[1]:
-                    $match['w'] = $gimgs[0];
+                if (!$m['w'] && $m['exist']) {
+                    ($m['height'])?
+                    $m['w'] = $m['height'] * $gimgs[0]/$gimgs[1]:
+                    $m['w'] = $gimgs[0];
                 }
-                $match['width'] = $match['w'];
-                $match['height'] = $match['h'];
-                if (!$match['align'] || $match['align']=='center'&&!$this->getConf('center_align')) {
-                    $match['align'] = 'rien';
+                $m['width'] = $m['w'];
+                $m['height'] = $m['h'];
+                if (!$m['align'] || $m['align']=='center' && !$this->getConf('center_align')) {
+                    $m['align'] = 'rien';
                 }
-                return array($state,$match);
+                return array($state, $m);
 
             case DOKU_LEXER_UNMATCHED:
-                return array($state,$match);
+                return array($state, $match);
 
             case DOKU_LEXER_EXIT:
-                return array($state,$match);
+                return array($state, '');
         }
     }
 
@@ -98,27 +98,28 @@ class syntax_plugin_imagebox extends DokuWiki_Syntax_Plugin {
      */
     function render($format, Doku_Renderer $renderer, $data) {
         if ($format == 'xhtml') {
-            list($state,$match) = $data;
+            list($state, $m) = $data;
 
             switch ($state) {
                 case DOKU_LEXER_ENTER:
-                    $renderer->doc.= '<div class="thumb2 t'.$match['align'].'">';
+                    $renderer->doc.= '<div class="thumb2 t'.$m['align'].'">';
                     $renderer->doc.= '<div class="thumbinner">';
-                    if ($match['exist']) {
-                        $renderer->{$match['type']}($match['src'],$match['title'],'box2',$match['width'],$match['height'],$match['cache'],$match['linking']);
+                    if ($m['exist']) {
+                        $renderer->{$m['type']}($m['src'],$m['title'],'box2',$m['width'],$m['height'],$m['cache'],$m['linking']);
                     } else {
                         $renderer->doc.= 'Invalid Link';
                     }
-                    $renderer->doc.= '<div class="thumbcaption" style="max-width: '.($match['width']-6).'px">';
-                    if ($match['detail']) {
+                    $renderer->doc.= '<div class="thumbcaption" style="max-width: '.($m['width']-6).'px">';
+                    if ($m['detail']) {
                         $renderer->doc.= '<div class="magnify">';
-                        $renderer->doc.= '<a class="internal" title="'.$this->getLang('enlarge').'" href="'.$match['detail'].'">';
+                        $renderer->doc.= '<a class="internal" title="'.$this->getLang('enlarge').'" href="'.$m['detail'].'">';
                         $renderer->doc.= '<img width="15" height="11" alt="" src="'.DOKU_BASE.'lib/plugins/imagebox/magnify-clip.png"/>';
                         $renderer->doc.= '</a></div>';
                     }
                     break;
 
                 case DOKU_LEXER_UNMATCHED:
+                    $match = $m;
                     $style=$this->getConf('default_caption_style');
                     if ($style=='Italic') {
                         $renderer->doc .= '<em>'.$renderer->_xmlEntities($match).'</em>';
