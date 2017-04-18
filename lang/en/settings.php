d817f726503ef8c62ff4bac64850283fbc99d1fd
Satoshi Sahara <sahara.satoshi@gmail.com>
2017-04-18 09:04:41 +0900

    add setting langage file for config manager

---
+++ lang/en/settings.php
@@ -0,0 +1,4 @@
+<?php
+$lang['display_magnify']       = 'Display the click-enlarge icon';
+//$lang['center_align']          = 'Allow center alignment of imaegbox';
+$lang['default_caption_style'] = 'Default style for image caption';
