35c5aa39fe73667450caf49605607521cd7aa1b7
Satoshi Sahara <sahara.satoshi@gmail.com>
2017-04-29 15:51:18 +0900

    added more box_style options

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,4 +1,19 @@
 <?php
-$meta['display_magnify'] = array('multichoice','_choices' => array('If necessary','Always','Never'));
-$meta['default_caption_style'] = array('multichoice','_choices' => array('Normal','Italic','Bold'));
-$meta['default_box_style'] = array('multichoice','_choices' => array('imagebox','imagebox0','imagebox1','imagebox2'));
+$meta['display_magnify'] = array('multichoice','_choices' => array(
+        'If necessary',
+        'Always',
+        'Never',
+));
+$meta['default_caption_style'] = array('multichoice','_choices' => array(
+        'Normal',
+        'Italic',
+        'Bold',
+));
+$meta['default_box_style'] = array('multichoice','_choices' => array(
+        'imagebox',
+        'imagebox0',
+        'imagebox1',
+        'imagebox1b',
+        'imagebox2',
+        'imagebox2b',
+));
